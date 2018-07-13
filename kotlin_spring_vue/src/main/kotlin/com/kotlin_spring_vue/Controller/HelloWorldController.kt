64408b078895c42fa53937467174ad5ccabbf7dd
Yoshiki <rrrtcdish2@gmail.com>
2018-07-13 18:41:12 +0900

    二重クラスだったので修正

--- kotlin_spring_vue/src/main/kotlin/com/kotlin_spring_vue/Controller/HelloWorldController.kt
+++ kotlin_spring_vue/src/main/kotlin/com/kotlin_spring_vue/Controller/HelloWorldController.kt
@@ -3,12 +3,8 @@ package com.kotlin_spring_vue.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+@RestController
 class HelloWorldController {
-
-    @RestController
-    class HelloWorldController {
-
-        @GetMapping("/hello")
-        fun hello() = "Hello Kotlin !"
-    }
+    @GetMapping("/hello")
+    fun hello() = "Hello Kotlin !"
 }
