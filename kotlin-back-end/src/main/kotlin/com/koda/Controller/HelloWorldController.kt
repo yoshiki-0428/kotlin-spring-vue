a9779dea61b0d1216bc907ccdcc965d34ab92ecb
Yoshiki <rrrtcdish2@gmail.com>
2018-07-15 00:25:00 +0900

    Add Controller method

--- kotlin-back-end/src/main/kotlin/com/koda/Controller/HelloWorldController.kt
+++ kotlin-back-end/src/main/kotlin/com/koda/Controller/HelloWorldController.kt
@@ -1,10 +1,25 @@
 package com.koda.Controller
 
+import com.koda.Entity.Todo
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HelloWorldController {
-    @GetMapping("/hello")
+    // イコールでつなげると返却する型を推定する
+    @GetMapping("hello")
     fun hello() = "Hello Kotlin !"
+
+    @GetMapping("list")
+    // fun: メソッド宣言 : 返却する値
+    fun getHelloList(): MutableList<String> {
+        // return mutableListOf("hello", "こんにちは", "コップンカー")
+        val helloList = mutableListOf("hello", "こんにちは", "コップンカー")
+        return helloList
+    }
+
+    @GetMapping("todo")
+    fun getTodo(): Todo {
+        return Todo(0, "git", "git clone")
+    }
 }
---
+++ kotlin-back-end/src/main/kotlin/com/koda/Entity/Todo.kt
@@ -0,0 +1,9 @@
+package com.koda.Entity
+
+// これだけでクラス宣言可能、同時にコンストラクタも生成している
+class Todo(
+        val id: Int,
+        val todoName: String,
+        val todoDetail: String
+) {
+}
